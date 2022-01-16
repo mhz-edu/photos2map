3154cc256e47b0e8c0d117c2af32e7ef540829d0
unknown <mhz.edu@gmail.com>
2022-01-16 20:48:47 +0200

    Extract gps coordinates from uploaded file

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const multer  = require('multer');
+const ExifImage = require('exif').ExifImage;
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works'
-    })
-})
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage });
+
+app.post('/api/file', upload.single('upfile'), ((req, res) => {
+    new Promise(function(resolve, reject) {
+        new ExifImage({image: req.file.buffer}, function (error, exifData) {
+            if (error) {
+                console.log('Error: '+error.message);
+                reject(error);
+            } else {
+                console.log(exifData.gps);
+                resolve(exifData.gps);
+            }
+        });
+    }).then(result => res.json(result), error => res.json(error));
+  }));
 
 module.exports = app;
