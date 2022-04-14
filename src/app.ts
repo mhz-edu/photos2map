73eceb14276bef82a56f6f19d76586f3ba856e3b
unknown <mhz.edu@gmail.com>
2022-04-14 23:40:15 +0300

    Moved error handler to separate file

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { ErrorRequestHandler, RequestHandler } from 'express';
 import usersRouter from './routes/users';
 import imagesRouter from './routes/images';
 import albumsRouter from './routes/albums';
+import errorHandler from './controllers/error';
 
 const app = express();
 
@@ -10,16 +11,6 @@ app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use(imagesRouter);
 app.use('/api/albums', albumsRouter);
-
-const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
-  const statusCode = error.statusCode || 500;
-  const data = error.data;
-  const message = error.message;
-  res.status(statusCode).json({
-    message: message,
-    error: data,
-  });
-};
 
 app.use(errorHandler);
 
